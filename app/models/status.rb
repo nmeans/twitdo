502340630a77ae73f3038404c0330230404ce84a
Cody Maggard <cody@heliumsyndicate.com>
2008-04-29 12:12:34 -0500

    Adding page parameter for scraping

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,7 @@
 class Status < ActiveResource::Base
   self.site = "http://need2:etacidnys@twitter.com"
   
-  def self.replies
-    find(:all, :from => '/replies.xml')
+  def self.replies(page = 1)
+    find(:all, :from => '/replies.xml', :params => { :page => page })
   end
 end
