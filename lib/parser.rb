f59f7ea1a85c5c06d1e289ecf659ce90203b31f0
Cody Maggard <cody@heliumsyndicate.com>
2008-04-29 13:39:43 -0500

    100% less infinite loops

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,18 +4,23 @@ module Parser
     last_tweet = ::Todo.find(:first, :order => "tweet_id DESC")
     last_id = last_tweet.nil? ? 1 : last_tweet.tweet_id
     
-    while 1
-      Status.replies(page).each do |st|
-        return if st.id.to_i >= last_id
+    catch :keep_going do
+      replies = Status.replies(page)
+      processed = replies.size
+      replies.each do |st|
+        break if st.id.to_i <= last_id
         add_todo(st)
+        processed = processed - 1
+        puts processed
       end
       page = page + 1
+      throw :keep_going unless processed != 0 || replies.empty?
     end
   end
   
   private
   
-  def add_todo(st)
+  def self.add_todo(st)
     account = Account.find_or_create_by_username(st.user.screen_name)
     if account.new_record?
       # Handle new accounts here
