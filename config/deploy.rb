5c945b5ef002adec0e0e618c5b30ac375535a09a
Nickolas Means <nick@heliumsyndicate.com>
2008-05-01 11:13:57 -0500

    Added Thin-specific tasks to deploy.rb.  Unpacked Haml 1.9.0 into repository.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ set :scm, :git
 set :repository,  "git@github.com:nmeans/twitdo.git"
 set :deploy_to, "/var/www/apps/twitdo"
 set :use_sudo, :true
+set :thin_conf, "/etc/thin/twitdo.yml"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -16,3 +17,44 @@ set :use_sudo, :true
 role :app, "twitdo-deploy@twitdo.heliumsyndicate.com"
 role :web, "twitdo-deploy@twitdo.heliumsyndicate.com"
 role :db,  "twitdo-deploy@twitdo.heliumsyndicate.com", :primary => true
+
+namespace :deploy do
+  namespace :thin do
+    [ :stop, :start, :restart ].each do |t|
+      desc "#{t.to_s.capitalize} the thin cluster"
+      task t, :roles => :app do
+        invoke_command "thin #{t.do_s} -C #{thin_conf}", :via => :sudo
+      end
+    end
+  end
+  
+  desc "Custom restart task for thin cluster"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    deploy.thin.restart
+  end
+
+  desc "Custom start task for thin cluster"
+  task :start, :roles => :app do
+    deploy.thin.start
+  end
+
+  desc "Custom stop task for thin cluster"
+  task :stop, :roles => :app do
+    deploy.thin.stop
+  end
+  
+  desc "Restart the web server"
+  task :restart_web, :roles => :web do
+    invoke_command "/etc/init.d/nginx restart", :via => :sudo
+  end
+
+  desc "Stop the web server"
+  task :stop_web, :roles => :web do
+    run "/etc/init.d/nginx stop", :via => :sudo
+  end
+
+  desc "Start the web server"
+  task :start_web, :roles => :web do
+    run "/etc/init.d/nginx start", :via => :sudo
+  end
+end
